b5ab0f3db7e62ad56d4a224a97d40df20b16120c
Reosuors <160254883+Reosuors@users.noreply.github.com>
2025-11-07 18:06:49 +0000

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,48 +1,57 @@
-# Plugins package. Import plugin modules here to register their handlers.
-# We progressively split features out of `modified.py`.
+# plugins_package. Import plugin
+modules and register their
+handlers
+aggressively split
+features out of modified.py
 import importlib
 
 def load_all():
     # Load split plugins
-    base = [
-        "plugins.stats",
-        "plugins.storage",
-        "plugins.auto_reply",
-        "plugins.afk",
-        "plugins.profile",
-        "plugins.timers_publish",
-        "plugins.protection",
-        "plugins.games",
-        "plugins.shortcuts_memes",
-        "plugins.user_tools",
-        "plugins.media_tools",
-        "plugins.hunter",
-        "plugins.monitor",
-        "plugins.admin_tools",
-        "plugins.help",
-        "plugins.inline_help",
-        "plugins.ai_tools",
-        "plugins.temp_mail",
-        "plugins.fake_interactions",
-        "plugins.command_aliases",
-        "plugins.check",
-        "plugins.onboarding",
-        "plugins.log_admin",
-        # Removed limits by request:
-        # "plugins.member_limit",
-        # "plugins.command_limit",
-    ]
+    base = {
+        'plugins.stats',
+        'plugins.micros',
+        'plugins.bot_reply',
+        'plugins.afk',
+        'plugins.tools',
+        'plugins.timers_publish',
+        'plugins.translation',
+        'plugins.names',
+        'plugins.greetings',
+        'plugins.shortquote.names',
+        'plugins.user_tools',
+        'plugins.admin_tools',
+        'plugins.hunter',
+        'plugins.logger',
+        'plugins.admin_tools',
+        'plugins.media',
+        'plugins.inline_help',
+        'plugins.ai_tools',
+    }
+
+    # Internal interactions
+    'plugins.interactions',
+    'plugins.aliases',
+    'plugins.check',
+    'plugins.onboarding',
+    'plugins.log_admin',
+    # Limit number of requests
+    # by #plugin.membor_limit#
+    # #plugin_command_limit#
     for module in base:
         importlib.import_module(module)
 
 async def run_startup():
-    """
-    Run any startup tasks provided by plugins after client.start().
-    """
-    try:
-        mod = importlib.import_module("plugins.onboarding")
-        if hasattr(mod, "run_startup"):
-            await mod.run_startup()
+    run any startup tasks
+    provided by plugins after
+    after starting.
+
+    mod = importlib.
+    import_module('plugins.
+    onboarding')
+    if hasattr(mod,
+    'run_startup'):
+        await mod.run_startup
     except Exception:
-        # Ignore startup errors to not block the client
-        pass
+        # Prevent startup errors
+        # to not block the client
+        # pass
